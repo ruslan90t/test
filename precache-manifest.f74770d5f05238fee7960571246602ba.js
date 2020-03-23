1d9b3a01bb8e5fe169e17f5605c693528a843616
ruslan90t <ruslan90t@yandex.ru>
2020-03-23 12:41:33 +0300

    Updates

---
+++ precache-manifest.f74770d5f05238fee7960571246602ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7afc1366ac8d768ddf271eff579d838",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "f3940a4af29b262b8348",
+    "url": "/test/static/css/main.203b07e6.chunk.css"
+  },
+  {
+    "revision": "2f75af8e44ef2edcc3f9",
+    "url": "/test/static/js/2.4bf9ed25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.4bf9ed25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3940a4af29b262b8348",
+    "url": "/test/static/js/main.9ba1243d.chunk.js"
+  },
+  {
+    "revision": "a2a118daa8ca02cff825",
+    "url": "/test/static/js/runtime-main.5ebfacb6.js"
+  }
+]);
