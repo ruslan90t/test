b7c13a97136b1ca0c2a3e2526c244fbcafc5722c
ruslan90t <ruslan90t@yandex.ru>
2020-03-23 12:27:16 +0300

    modif

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid">
+   <div className="header">
+     <div className="logo">
+       <img src="https://cdn3.sportngin.com/attachments/photo/7734/8653/Spartans_2100_1300_large.png"/>
+     </div>
+   </div>
+   <div className="side">
+     <ul>
+     <li><a href="#">О нас</a></li>
+     <li><a href="#">Контакты</a></li>
+     <li><a href="#">Стратегия</a></li>
+     <li><a href="#">О компании</a></li>
+     <li><a href="#">Справка</a></li>
+     </ul>
+   </div>
+   <div className="content">
+     <img src="http://wallpapers-image.ru/1280x800/cars/wallpapers/wallpapers-cars-09.jpg"/>
+   </div>
     </div>
   );
 }
